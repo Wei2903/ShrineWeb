56a70a71de047aef5a16ef1cbdaea266d9cd853b
Songtao Zhu <u6655955@anu.edu.au>
2023-08-21 14:26:49 +0800

    test CI settings

--- ShrineWeb/src/main/java/com/shrine/web/HelloShrine.java
+++ ShrineWeb/src/main/java/com/shrine/web/HelloShrine.java
@@ -2,6 +2,6 @@ package com.shrine.web;
 
 public class HelloShrine {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("test");
     }
 }
